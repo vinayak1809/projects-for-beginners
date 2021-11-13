2adddc0c35cee5edcb07dfdc289b78f654a2f8d7
vinayak1809 <vinayakrshinde18@gmail.com>
2021-11-13 23:40:32 +0530

    file upload

---
+++ stone-paper-scissor.py
@@ -0,0 +1,42 @@
+import random
+
+loop = False
+
+
+def game():
+    pla_point = 0
+    com_point = 0
+    while not loop:
+        li = ["stone", "paper", "scissor"]
+        computer = random.choice(li)
+
+        player = input("\nStone Paper or Scissor : ").lower()
+        print(f"\ncomputer's move : {computer}")
+
+        if player == computer:
+            print("tied")
+
+        elif (player == "stone" and computer == "scissor") or (player == "paper" and computer == "stone") or (player == "scissor" and computer == "paper"):
+
+            pla_point += 1
+            print(f"\nPoints : Player: {pla_point} and Computer: {com_point} ")
+
+            if pla_point == 3:
+                print("Player won :) ")
+
+        else:
+            com_point += 1
+            print(f"Points : Computer: {com_point} and Player: {pla_point} ")
+
+            if com_point == 3:
+                print("\ncomputer won :( ")
+
+        if pla_point == 3 or com_point == 3:
+            ask = input("\ndo u wanna play again(Y/N) : ").lower()
+            if ask == "y":
+                game()
+            else:
+                exit()
+
+
+game()
