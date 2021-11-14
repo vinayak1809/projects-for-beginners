fe5b3b3c1e33add360245d45ea9bb2073f1fd442
vinayak1809 <vinayakrshinde18@gmail.com>
2021-11-14 12:11:15 +0530

    player input correction

--- stone-paper-scissor.py
+++ stone-paper-scissor.py
@@ -11,32 +11,36 @@ def game():
         computer = random.choice(li)
 
         player = input("\nStone Paper or Scissor : ").lower()
-        print(f"\ncomputer's move : {computer}")
+        if player in li:
+            print(f"\ncomputer's move : {computer}")
 
-        if player == computer:
-            print("tied")
+            if player == computer:
+                print("tied")
 
-        elif (player == "stone" and computer == "scissor") or (player == "paper" and computer == "stone") or (player == "scissor" and computer == "paper"):
+            elif (player == "stone" and computer == "scissor") or (player == "paper" and computer == "stone") or (player == "scissor" and computer == "paper"):
 
-            pla_point += 1
-            print(f"\nPoints : Player: {pla_point} and Computer: {com_point} ")
+                pla_point += 1
+                print(
+                    f"\nPoints : Player: {pla_point} and Computer: {com_point} ")
 
-            if pla_point == 3:
-                print("Player won :) ")
+                if pla_point == 3:
+                    print("Player won :) ")
+
+            else:
+                com_point += 1
+                print(
+                    f"Points : Computer: {com_point} and Player: {pla_point} ")
+
+                if com_point == 3:
+                    print("\ncomputer won :( ")
+
+            if pla_point == 3 or com_point == 3:
+                ask = input("\nenter to play again : ").lower()
+                if ask:
+                    game()
 
         else:
-            com_point += 1
-            print(f"Points : Computer: {com_point} and Player: {pla_point} ")
-
-            if com_point == 3:
-                print("\ncomputer won :( ")
-
-        if pla_point == 3 or com_point == 3:
-            ask = input("\ndo u wanna play again(Y/N) : ").lower()
-            if ask == "y":
-                game()
-            else:
-                exit()
+            print("guess \"stone, paper / scissor \"")
 
 
 game()
